7f1f72a9e558aa192c97f9641f7eb8ff5f0009a6
abraiyan <abraiyan20@gmail.com>
2020-11-01 14:04:27 +0600

    FC

---
+++ android/app/src/main/kotlin/com/example/sideappbarui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sideappbarui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int bottomNavBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Container(
+        padding: EdgeInsets.only(left: 8, top: 10, bottom: 10, right: 12),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.add, color: Colors.white, size: 20,),
+            SizedBox(width: 6),
+            Text('Add Task', style: GoogleFonts.montserrat(color: Colors.white, fontSize: 16),)
+          ],
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 6,
+              offset: Offset(1.0, 2.0),
+            ),
+          ],
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: bottomNavBarIndex,
+      onTap: (index) {
+        setState(() {
+          bottomNavBarIndex = index;
+        });
+      },
+      selectedFontSize: 18.0,
+      unselectedFontSize: 14.0,
+      selectedLabelStyle: GoogleFonts.montserrat(),
+      unselectedLabelStyle: GoogleFonts.montserrat(),
+      backgroundColor: Colors.white,
+      items: [
+        BottomNavigationBarItem(
+          icon: SizedBox(),
+          label: "Todo",
+        ),
+        BottomNavigationBarItem(
+          icon: SizedBox(),
+          label: "Doing",
+        ),
+        BottomNavigationBarItem(
+          icon: SizedBox(),
+          label: "Done",
+        ),
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      toolbarHeight: 70,
+      elevation: 0,
+      backgroundColor: Colors.white,
+      title: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundImage: AssetImage('assets/03w.jpg'),
+          ),
+          SizedBox(width: 12,),
+          Text('Hello Kitty', style: GoogleFonts.montserrat(color: Colors.black87, fontSize: 16),),
+        ],
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.more_vert, color: Colors.black87,),
+        ),
+      ],
+    );
+  }
+}
+
