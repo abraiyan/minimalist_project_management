cc1b1fc4ec289a8ff6333de42f47682ed6ab55aa
abraiyan <abraiyan20@gmail.com>
2020-11-18 23:51:31 +0600

    Sequence Edited

--- lib/constants/constant_color.dart
+++ lib/constants/constant_color.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const Color kColorChipHigh = Color(0xffFF005E); //High Chip Color
-  static const Color kColorChipMedium = Color(0xffA900FF); //High Chip Color
-  static const Color kColorChipLow = Color(0xff00B2FF);
+  static const Color kColorChipMedium = Color(0xffA900FF); //Medium Chip Color
+  static const Color kColorChipLow = Color(0xff00B2FF); //Low Chip Color
 
   static const List<String> kTitles = [
     'To Do',
