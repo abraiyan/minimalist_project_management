3119f460d5401d66cd05357110545c4bfaa378d2
abraiyan <abraiyan20@gmail.com>
2020-11-30 20:18:14 +0600

    Some Comments Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sideappbarui/screens/home_page.dart';
+import 'package:sideappbarui/screens/onboarding_screen.dart';
 import 'package:sideappbarui/services/database.dart';
 
-void main() {
+int intiScreen;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
+  intiScreen = preferences.getInt('initScreen');
+  await preferences.setInt('initScreen', 1);
   runApp(MyApp());
 }
 
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
       create: (context) =>  AppDatabase().itemsDao,
       child: MaterialApp(
         debugShowCheckedModeBanner: false, //To hide the DEBUG red bar from the app
-        home: HomePage(),
+        home: OnBoardingScreen(),
       ),
     );
   }
---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Minimalist ',
+                  style: GoogleFonts.montserrat(color: Colors.black87, fontSize: 30, fontWeight: FontWeight.w500),
+                  children: [
+                    TextSpan(
+                      text: 'To Do',
+                      style: GoogleFonts.montserrat(color: Colors.black87, fontSize: 30),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/onboard_one.png'),
+                  ),
+                ),
+              ),
+              Text('A Simple Task Management App\nMade By - AB Raiyan', textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.black87, fontSize: 18),),
+              const SizedBox(height: 20,),
+              FlatButton(
+                onPressed: () {
+
+                },
+                padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+                color: Colors.blueAccent,
+                child: Text('Start', style: GoogleFonts.montserrat(color: Colors.white, fontSize: 20),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
